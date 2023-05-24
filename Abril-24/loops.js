ffbe591d6060b0cace7b8d9856b2a305f6e0a90d
Filadelico <nestor_05ribero@hotmail.com>
2023-05-24 13:01:17 -0500

    update

---
+++ Abril-24/loops.js
@@ -0,0 +1,28 @@
+
+
+/*
+for (let idx; idx = 0; idx++) {
+
+}
+
+let inc = 0;
+while (inc == 0) {
+
+
+    inc++;
+}//
+*/
+
+var lista = [1, `"nestor's Bar"`, false];//mixta
+var nums = [3, 4, 7, 12, 14, 26, 45, 78];
+console.log(lista);
+
+console.log(nums[0]);
+console.log(nums[1]);
+console.log(nums[2]);
+console.log(nums[3]);
+console.log(nums[4]);
+
+for (let index = 0; index < nums.length; index++) {
+    console.log(nums[index]);
+}
---
+++ Abril-24/script.js
@@ -0,0 +1,30 @@
+// texto numericas boleanas
+
+
+var nombre = "Nestor";
+var edad = 25;
+var estatura = 1.85;
+var graduado = true;
+
+
+
+console.log(nombre + " Ribero");
+console.log(nombre + edad);
+console.log(nombre + edad + estatura);
+console.log(edad + estatura + nombre);
+console.log(nombre + (edad + estatura));
+
+// <p><span>15</span> likes<p>
+
+let likes = "15.14";
+let numero = parseInt(likes);
+let decimal = parseFloat(likes);
+
+likes = likes + 1;
+console.log(likes);//
+
+numero = numero + 1;
+console.log(numero);//
+
+decimal = decimal + 1;
+console.log(decimal);//
