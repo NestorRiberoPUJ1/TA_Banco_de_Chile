7b09aa5a1ebd5742d7c0b20e500fda6871c2ac4e
Filadelico <nestor_05ribero@hotmail.com>
2023-06-05 12:13:35 -0500

    update

--- Junio-1/script.js
+++ Junio-1/script.js
@@ -6,7 +6,7 @@ const aumentar = (element) => {
     element.innerHTML = num;
 }
 
-
+/*
 setTimeout(() => {
     let numeroRojo = document.getElementById("redTeam");
     let numeroAzul = document.getElementById("blueTeam");
@@ -22,9 +22,42 @@ setTimeout(() => {
         alert("Empate");
     }
 }, 10000)
-
+*/
 
 function solicitarImagen() {
-    //fetch()
+    fetch("https://dog.ceo/api/breed/hound/images/random", {
+        method: "GET",
+    })
+        .then((response) => response.json())
+        .then((result) => {
+            console.log(result);
+            let imagenDog = document.getElementById("imagenDog");
+            imagenDog.src = result.message;
+            console.log(result.message);
+        })
+        .catch(error => console.log(error));
 }
 
+async function buscarPerrito() {
+    try {
+        const response = await fetch("https://dog.ceo/api/breed/akita/images/random", {
+            method: "GET",
+        });
+        const result = await response.json();
+        console.log(result);
+        let imagenDog = document.getElementById("imagenDog");
+        imagenDog.src = result.message;
+        console.log(result.message);
+    }
+    catch (error) {
+        console.log("AWAIT", error);
+    }
+
+}
+
+var etiqueta = document.getElementById("imagenDog");
+var arreglo = [1, 2, 3, 4, 5];
+
+var doble = arreglo.map((gato,idx,lista) => {
+    console.log(gato,idx,lista);
+})
